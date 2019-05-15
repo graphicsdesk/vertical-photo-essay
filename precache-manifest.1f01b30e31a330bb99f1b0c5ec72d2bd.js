98cc4843c5ae83907ce163d2f960121d7931e819
Jason Kao <jason.kao@columbia.edu>
2019-05-15 11:04:25 -0400

    Updates

---
+++ precache-manifest.1f01b30e31a330bb99f1b0c5ec72d2bd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72869e8bde582aa9c7ddead880ed2a84",
+    "url": "/vertical-photo-essay/index.html"
+  },
+  {
+    "revision": "5c014063a0d2f0cc3439",
+    "url": "/vertical-photo-essay/static/js/2.9fac1af1.chunk.js"
+  },
+  {
+    "revision": "e99709e196a1d3115b2f",
+    "url": "/vertical-photo-essay/static/js/main.cfcb963b.chunk.js"
+  },
+  {
+    "revision": "95e9edbc38edd8da6b31",
+    "url": "/vertical-photo-essay/static/js/runtime~main.cc82ad63.js"
+  }
+]);
